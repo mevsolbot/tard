e78c6755ac64b6b44841ea30dc198190293b2240
mevsolbot <mevmoonshotbot@gmail.com>
2024-07-08 09:43:20 -0400

    Add files via upload

---
+++ retard.js
@@ -0,0 +1,9 @@
+function functionYes(){
+    document.getElementById("applePic1").src= "file:///C:/Users/tenzi/Downloads/s6.png";
+
+}
+function functionEx(){
+    document.getElementById("applePic1").src= "file:///C:/Users/tenzi/Downloads/s7.png";
+    document.getElementById("first").style.display = "none";
+
+}
